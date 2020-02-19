0356a3a531634e821856ee4ba9d309f890261f52
Chuck Bennett <cnbennett3@gmail.com>
2020-02-19 16:26:35 -0500

    Refactored csvExecutor into smaller methods.

--- graphLoader.scala
+++ graphLoader.scala
@@ -2,6 +2,7 @@ import org.opencypher.morpheus.api.MorpheusSession
 import org.opencypher.morpheus.api.io.{Node, Relationship, RelationshipType}
 import org.opencypher.okapi.api.io.conversion.{ElementMapping, NodeMappingBuilder, RelationshipMappingBuilder}
 import org.opencypher.morpheus.api.io.{MorpheusNodeTable, MorpheusRelationshipTable, MorpheusElementTable}
+import org.opencypher.okapi.api.graph.{PropertyGraph}
 import scala.collection.mutable.ListBuffer
 import org.apache.spark.sql.{DataFrame, Row, Dataset, SparkSession}
 
@@ -38,6 +39,11 @@ import java.util.concurrent.atomic.AtomicLong
 //    Ans: Leave this way for CSV (modify JSON list to be the same (?))
 //         For CSV, do partial string search using contains.
 //
+// 4) See if there's a better way to force UDF evaluation than what's currently done in
+//    triggerUDFEvaluation.Scala wants to lazily evaluate so UDFs don't 
+//    actually execute until after the dataframe is split, causing the globally unique
+//    id's to lose their uniqueness. From my research, so far, this is the only way to force 
+//    UDF's to run, though it's ridiculously costly in realtime.
 // ---------------------------------------------------------------------------------------
 
 // Morpheus: https://github.com/opencypher/morpheus
@@ -101,7 +107,6 @@ object csvFileDataSource extends DataSource {
       val tmpDf_w_src = tmpDf_w_id.withColumn("source", lit(0L))
       val tmpDf_w_tgt = tmpDf_w_src.withColumn("target", lit(0L))
       val tmpDf_ord = tmpDf_w_tgt.select("id", "source", "target", "subject", "object", "relation", "predicate_id", "relation_label")
-      //tmpDf_w_tgt.select("id", "source", "target", "subject", "obj", "relation", "predicate_id", "relation_label")
 
       // TODO: This needs to be generalized and moved to an index_data method!!! (See TODO #1)
       val tmpDf_relsrc = tmpDf_ord.withColumn("source", when(col("subject").equalTo("HGNC:12442"), 0).otherwise(col("source")))
@@ -126,88 +131,44 @@ object csvFileDataSource extends DataSource {
          tmpDF.withColumn("id", callUDF("nextNodeIdUDF", $"id"))
       }
    }
-
-   //def setEdgeSourceAndTargetCols(inEdgeDf: DataFrame, inNodeDf: DataFrame): DataFrame = {
-      //val tmpDf_relsrc = tmpDf_ord.withColumn("source", when(col("subject").equalTo("HGNC:12442"), 0).otherwise(col("source")))
-      //tmpDf_relsrc.withColumn("target", when(col("object").equalTo("MONDO:0002022"), 1).otherwise(col("target")))
-
-      //val tmpEdgeDf = inEdgeDf.withColumn("source", when(inEdgeDf.col("subject").equalTo(inNodeDf.col("curie_or_id"), inNodeDf.col("id")).otherwise(0)))
-      //tmpEdgeDf.withColumn("target", when(tmpEdgeDf.col("obj").equalTo(inNodeDf.col("curie_or_id"), inNodeDf.col("id")).otherwise(0)))
-   //}
-
 }
 
 object csvExecutor {
 
-   def execute(): Unit = {
-      // break this into individual methods.
-
-      //------------------------------------------------------------------------------
-      // Edge Data:
-      //------------------------------------------------------------------------------
+   def prepareEdgeData(): DataFrame = {
       val edgesDfFromFile = csvFileDataSource.readData("target/edges_max.csv")
       val edgesDf = csvFileDataSource.cleanEdgeData(edgesDfFromFile)
       edgesDf.show(10000, false)
+      edgesDf
+   }
 
-      //------------------------------------------------------------------------------
-      // Node Data:
-      //------------------------------------------------------------------------------
+   def prepareNodeData(): DataFrame = {
       val nodesDfFromFile = csvFileDataSource.readData("target/nodes_max.csv")
       val nodesDf = csvFileDataSource.cleanNodeData(nodesDfFromFile)
       nodesDf.show(125, false)
+      nodesDf
+   }
 
-      println ("    Show edge and node schemas . . .")
-      edgesDf.schema.printTreeString
-      nodesDf.schema.printTreeString
+   def triggerUDFEvaluation(df: DataFrame, s: String): DataFrame = {
+      var fileWriteStr = "target/" + s + ".csv"
+      var fileReadStr = fileWriteStr + "/part*.csv"
+      csvFileDataSource.writeData(df, fileWriteStr)
+      val newDf = csvFileDataSource.readData(fileReadStr)
+      println("   NEWLY READ IN df FROM FILE:")
+      newDf.show(2000)
+      newDf
+   }
 
+   def createFilteredFrame(df: DataFrame, category: String, nodeTypeCol: String, nodeType: String): DataFrame = {
+      val filteredDf:DataFrame = df.filter($"category".contains(category))
+      val filteredDf2 = filteredDf.withColumn(nodeTypeCol, lit(nodeType))
+      println("   FILTERED DATAFRAME:")
+      filteredDf2.show(1000)
+      filteredDf2
+   }
 
-      // This code is required to actually trigger the UDF's that were called earlier that 
-      // were intended to create unique id's. Scala wants to lazily evaluate so they don't 
-      // get called until after the dataframe is split, causing the global uniqueness to
-      // get lost.  From my research, this is the only way to force UDF's to run, though
-      // it's ridiculously costly in realtime: write to disk and read back in.
-      // I'm going to write a defect for this and hopefully it will change.
+   def executeQueries(graph: PropertyGraph): Unit = {
 
-      // methodify this:
-
-      csvFileDataSource.writeData(nodesDf, "target/nodes_cleaned.csv")
-      val new_nodesDf = csvFileDataSource.readData("target/nodes_cleaned.csv/part*.csv")
-      println("   NEWLY READ IN nodesdf FROM FILE:")
-      new_nodesDf.show(2000)
-
-      csvFileDataSource.writeData(edgesDf, "target/edges_cleaned.csv")
-      val new_edgesDf = csvFileDataSource.readData("target/edges_cleaned.csv/part*.csv")
-      println("   NEWLY READ IN edgesdf FROM FILE:")
-      new_edgesDf.show(10000)
-
-
-      // Select rows of df that are gene-specific based on category to create a gene-only dataframe
-      println("   Create gene-only dataframe . . . ")
-      val geneDf:DataFrame = new_nodesDf.filter($"category".contains("named_thing|gene"))
-      val geneDf2 = geneDf.withColumn("gene_node_type", lit("gene"))
-      println("   FILTERED geneDf:")
-      geneDf2.show(1000)
-
-      // Select rows of df that are disease-specific based on category to create a disease-only dataframe
-      println("   Create disease-only dataframe . . . ")
-      val diseaseDf:DataFrame = new_nodesDf.filter($"category".contains("named_thing|disease"))
-      val diseaseDf2 = diseaseDf.withColumn("disease_node_type", lit("disease"))
-      println("   FILTERED diseaseDf:")
-      diseaseDf2.show(1000)
-
-      println ("   Initialize Morpheus...")
-      implicit val morpheus: MorpheusSession = MorpheusSession.local()
-
-
-      println("   Create Morpheus tables from dataframes . . .")
-      val geneTable = MorpheusNodeTable(Set("Gene"), geneDf2)
-      val diseaseTable = MorpheusNodeTable(Set("Disease"), diseaseDf2)
-      val geneToDiseaseRelationshipTable = MorpheusRelationshipTable("GENE_TO_DISEASE", new_edgesDf.toDF())
-
-      println("   Create graph from Morpheus tables . . . ")
-      val graph = morpheus.readFrom(geneTable, diseaseTable, geneToDiseaseRelationshipTable)
-
-      // Execute queries
       println("\n\n\nQuerying: MATCH (g:Gene)-[r]-(d:Disease) RETURN g.name, g.curie_or_id, r.relation_label, d.name, d.curie_or_id\n\n")
       var r = graph.cypher("MATCH (g:Gene)-[r]-(d:Disease) RETURN g.name, g.curie_or_id, r.relation_label, d.name, d.curie_or_id")
       r.show
@@ -227,16 +188,42 @@ object csvExecutor {
       println("\n\nQuerying: MATCH (g:Gene {name: 'ENSG00000077498'})-[r {relation_label: 'pathogenic_for_condition'}]-(d:Disease) RETURN d\n\n")
       r = graph.cypher("MATCH (g:Gene {name: 'ENSG00000077498'})-[r {relation_label: '[pathogenic_for_condition]'}]-(d:Disease) RETURN d")
       r.show
+   }
+
+   def execute(): Unit = {
+
+      val edgesDf = prepareEdgeData()
+      val nodesDf = prepareNodeData()
+
+      println ("    Show edge and node schemas . . .")
+      edgesDf.schema.printTreeString
+      nodesDf.schema.printTreeString
+
+      val new_nodesDf = triggerUDFEvaluation(nodesDf, "nodes_cleaned")
+      val new_edgesDf = triggerUDFEvaluation(edgesDf, "edges_cleaned")
+
+      val geneDf2 = createFilteredFrame(new_nodesDf, "named_thing|gene", "gene_node_type", "gene")
+      val diseaseDf2 = createFilteredFrame(new_nodesDf, "named_thing|disease", "disease_node_type", "disease")
+
+      println ("   Initialize Morpheus...")
+      implicit val morpheus: MorpheusSession = MorpheusSession.local()
+
+      println("   Create Morpheus tables from dataframes . . .")
+      val geneTable = MorpheusNodeTable(Set("Gene"), geneDf2)
+      val diseaseTable = MorpheusNodeTable(Set("Disease"), diseaseDf2)
+      val geneToDiseaseRelationshipTable = MorpheusRelationshipTable("GENE_TO_DISEASE", new_edgesDf.toDF())
+
+      println("   Create graph from Morpheus tables . . . ")
+      val graph = morpheus.readFrom(geneTable, diseaseTable, geneToDiseaseRelationshipTable)
+
+      executeQueries(graph)
 
       println("Cleaning up temp files . . .")
-      val r = Seq("/bin/sh", "-c", "rm -rf target/*_cleaned.csv").!!
+      val r2 = Seq("/bin/sh", "-c", "rm -rf target/*_cleaned.csv").!!
 
-      println("")
-      println("")
-      println("========================= DONE ============================")
-      println("")
-      println("")
+      println("\n\n========================= DONE ============================\n\n")
    }
 }
 
 csvExecutor.execute
+
