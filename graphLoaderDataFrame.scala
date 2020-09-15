43b0ca31baa674eefe47c1680a066f2b493fa387
Yaphetkg <kebedey@renci.org>
2020-09-15 17:08:29 -0400

    cypher timings

--- graphLoaderDataFrame.scala
+++ graphLoaderDataFrame.scala
@@ -2,7 +2,10 @@ import org.apache.spark.sql.DataFrame
 import org.apache.spark.sql.functions.col
 import org.opencypher.morpheus.api.MorpheusSession
 import org.opencypher.morpheus.api.io.MorpheusElementTable
+import org.opencypher.morpheus.impl.table.SparkTable
 import org.opencypher.okapi.api.io.conversion.{ElementMapping, NodeMappingBuilder, RelationshipMappingBuilder}
+import org.opencypher.okapi.relational.api.graph.RelationalCypherGraph
+import org.opencypher.okapi.relational.api.table.Table
 
 import scala.collection.mutable.WrappedArray
 
@@ -51,7 +54,7 @@ object KGXNodesFileReader extends KGXFileReader {
   }
 }
 
-object KGXEdgesReader extends KGXFileReader {
+object KGXEdgesFileReader extends KGXFileReader {
   override def createElementTables(filePath: String, session: MorpheusSession): Seq[MorpheusElementTable] = {
     /**
      *
@@ -97,17 +100,67 @@ object KGXEdgesReader extends KGXFileReader {
 implicit val morpheus: MorpheusSession = MorpheusSession.local()
 // Morpheus sesssion
 
-val nodeFileName = "./test_small_files/nodes_small.json"
-val edgesFileName = "./test_small_files/edges_small.json"
+//val nodeFileName = "./test_small_files/nodes_small.json"
+//val edgesFileName = "./test_small_files/edges_small.json"
+//
+//val nodeElements = KGXNodesFileReader.createElementTables(nodeFileName, morpheus)
+//val edgeElements = KGXEdgesFileReader.createElementTables(edgesFileName, morpheus)
+//
+//val allElements: Seq[MorpheusElementTable] = nodeElements ++ edgeElements
+//val graph2= morpheus.readFrom(allElements(0), allElements.slice(1, allElements.length): _*)
+//graph2.cache()
+////// Test to see if all went well
+//val result = graph2.cypher("Match ()-[e]-() return e limit 10")
+//result.records.table.df.show()
 
-val nodeElements = KGXNodesFileReader.createElementTables(nodeFileName, morpheus)
-val edgeElements = KGXEdgesReader.createElementTables(edgesFileName, morpheus)
 
-val allElements: Seq[MorpheusElementTable] = nodeElements ++ edgeElements
-val graph2= morpheus.readFrom(allElements(0), allElements.slice(1, allElements.length): _*)
-graph2.cache()
-//// Test to see if all went well
-val result = graph2.cypher("Match ()-[e]-() return e limit 10")
-result.records.table.df.show()
+var nodeElements: Seq[MorpheusElementTable] = Seq[MorpheusElementTable]()
+var edgeElements: Seq[MorpheusElementTable] = Seq[MorpheusElementTable]()
+val basePath = "/home/yaphet"
+val nodeFileNames = Seq(
+  basePath + "/test_data_t2/Human_GOA_node_file.json",
+  basePath + "/test_data_t2/intact_node_file.json",
+  basePath + "/test_data_t2/Viral_proteome_GOA_node_file.json")
+val edgeFileNames = Seq(
+  basePath + "/test_data_t2/Human_GOA_edge_file.json",
+  basePath + "/test_data_t2/intact_edge_file.json",
+  basePath + "/test_data_t2/Viral_proteome_GOA_edge_file.json")
 
+for (fileName <- nodeFileNames){
+  println("***************************")
+  val start = System.currentTimeMillis()
+  val elements = KGXNodesFileReader.createElementTables(fileName, morpheus)
+  nodeElements = nodeElements ++ elements
+  print("creating element table for"+ fileName + " took: ")
+  print(System.currentTimeMillis() - start )
+  println("(ms)")
+  println("***************************")
+}
+for (fileName <- edgeFileNames){
+  println("***************************")
+  val start = System.currentTimeMillis()
+  val elements = KGXEdgesFileReader.createElementTables(fileName, morpheus)
+  print("creating element table for"+ fileName + " took: ")
+  print(System.currentTimeMillis() - start)
+  edgeElements = edgeElements ++ elements
+  println("(ms)")
+  println("***************************")
+}
+object cypherRunner {
+  def run_cypher_and_show(query: String, graph: RelationalCypherGraph[SparkTable.DataFrameTable]): Unit = {
+    val start = System.currentTimeMillis()
+    val result = graph.cypher(query).records.table.df.show
+    print("Running query: ")
+    println(query)
+    print("took")
+    print(System.currentTimeMillis() - start)
+    println("ms")
+    result
+  }
+}
+
+
+val allElementTables = nodeElements ++ edgeElements
+
+val graph = morpheus.readFrom(allElementTables(0), allElementTables.slice(1, allElementTables.length): _*)
 
