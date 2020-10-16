02ca22a4edbdc9a9f0f821682eba01c9c7b9d5a2
Yaphetkg <kebedey@renci.org>
2020-10-16 09:11:00 -0400

    driver mem and cores conf

--- app/services/T2Service.scala
+++ app/services/T2Service.scala
@@ -24,6 +24,16 @@ class T2Service @Inject() (config: Configuration) {
 //      "spark.executor.instances", config.get[String]("t2.spark.executor.instances")
 //    ).set(
 //      "spark.kubernetes.container.image", config.get[String]("t2.spark.kubernetes.container.image")
+    ).set(
+      "spark.driver.memory", config.get[String]("t2.spark.driver.memory")
+    ).set(
+      "spark.driver.maxResultSize", config.get[String]("t2.spark.driver.memory")
+    ).set(
+      "spark.driver.cores", config.get[String]("t2.spark.driver.cores")
+    ).set(
+      "spark.executor.cores", config.get[String]("t2.spark.executor.cores")
+    ).set(
+      "spark.local.dir", config.get[String]("t2.spark.local.dir")
     )
     // Voila we are set make core
     val kgxFileServer = config.get[String]("t2.kgx.serverRoot")
